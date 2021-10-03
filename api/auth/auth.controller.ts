29e780742cde3968e661c7c343ffc2664a5a3b39
whitedevil31 <magz3116@gmail.com>
2021-10-03 18:25:06 +0530

    FIx auth issue

--- api/auth/auth.controller.ts
+++ api/auth/auth.controller.ts
@@ -1,74 +1,74 @@
 import * as mongodb from "mongodb";
 import { userType, userSchema } from "./auth.schema";
 import HttpError from "http-errors";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import { getClient } from "../db/db.connect";
 
 export const registerUser = async (userData: userType) => {
+  try {
+    const client: mongodb.MongoClient = await getClient();
+    const DB = await client.db().collection("users");
 
-    try {
-        const client: mongodb.MongoClient = await getClient();
-        const DB = await client.db().collection("users");
-
-        if (await DB.findOne({ name: userData.name })) {
-            throw HttpError(409, "User already exists!")
-        }
-
-        const hashedPassword = await bcrypt.hash('test', 10)
-
-        const newUserData = {
-            name: userData.name,
-            password: hashedPassword,
-            email: userData.email,
-            phone: userData.phone
-        }
-
-
-
-        console.log(newUserData)
-
-        const response = await DB.insertOne(newUserData);
-
-
-        if (!response) {
-            throw HttpError(500, "Internal Server Error!")
-        }
-
-        return {
-            success: true,
-            _id: response.insertedId,
-
-        }
-
-    } catch (err) {
-        throw err;
+    if (await DB.findOne({ name: userData.name })) {
+      throw HttpError(409, "User already exists!");
     }
-}
+    const salt = await bcrypt.genSalt(12);
+    console.log(userData.password);
+    const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+    const newUserData = {
+      name: userData.name,
+      password: hashedPassword,
+      email: userData.email,
+      phone: userData.phone,
+    };
+
+    console.log(newUserData);
+
+    const response = await DB.insertOne(newUserData);
+
+    if (!response) {
+      throw HttpError(500, "Internal Server Error!");
+    }
+
+    return {
+      success: true,
+      _id: response.insertedId,
+    };
+  } catch (err) {
+    throw err;
+  }
+};
 
 export const loginUser = async (email: string, password: string) => {
+  try {
+    const client: mongodb.MongoClient = await getClient();
+    console.log(email);
 
-    try {
-        const client: mongodb.MongoClient = await getClient();
-        const verifyUser = await client.db().collection('users').findOne({ email: email })
+    const verifyUser = await client
+      .db()
+      .collection("users")
+      .findOne({ email: email });
 
-        console.log(verifyUser)
-
-        if (!verifyUser) {
-            throw HttpError(401, "Email does not exist, please create a new account!")
-        }
-
-        const correctPassword = await bcrypt.compare(password, verifyUser.password)
-
-        if (!correctPassword) {
-            throw HttpError(401, "Password Incorrect, please try again.")
-        }
-
-        return {
-            success: true,
-            status: 201
-        }
-
-    } catch (err) {
-        throw err
+    if (!verifyUser) {
+      throw HttpError(
+        401,
+        "Email does not exist, please create a new account!"
+      );
     }
-}
+    console.log(password);
+    console.log(verifyUser.password);
+    const correctPassword = await bcrypt.compare(password, verifyUser.password);
+    console.log(correctPassword);
+    if (!correctPassword) {
+      throw HttpError(401, "Password Incorrect, please try again.");
+    }
+
+    return {
+      success: true,
+      status: 201,
+    };
+  } catch (err) {
+    throw err;
+  }
+};
--- api/auth/auth.routes.ts
+++ api/auth/auth.routes.ts
@@ -1,50 +1,51 @@
 import { Router, Request, Response, NextFunction } from "express";
-import { registerUser, loginUser } from './auth.controller'
+import { registerUser, loginUser } from "./auth.controller";
 
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
+const router: Router = Router();
 
-const router: Router = Router()
-
-// router.post('/api/register', async (req: Request, res: Response, next: NextFunction) => {
-//     const userData = req.body;
-
-//     try {
-//         const result = await registerUser(userData)
-//         res.json(result).status(201)
-
-//     } catch (err) {
-//         throw err;
-//     }
-// })
-
-router.post('/api/login', async (req: Request, res: Response, next: NextFunction) => {
-    const { password, email } = req.body
+router.post(
+  "/api/register",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userData = req.body;
 
     try {
-
-        const result = await loginUser(password, email)
-
-        const token = jwt.sign(
-            {
-                email: email.email,
-            },
-            process.env.JWT_SECRET || '',
-            {
-                expiresIn: "10d",
-            }
-        );
-
-        res.json({
-            result,
-            token,
-            message: 'User successfully logged in!'
-
-        })
+      const result = await registerUser(userData);
+      res.json(result).status(201);
     } catch (err) {
-        throw err
+      throw err;
     }
-})
+  }
+);
 
+router.post(
+  "/api/login",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { password, email } = req.body;
 
-export default router
+    try {
+      const result = await loginUser(email, password);
+
+      const token = jwt.sign(
+        {
+          email: email.email,
+        },
+        process.env.JWT_SECRET || "",
+        {
+          expiresIn: "10d",
+        }
+      );
+
+      res.json({
+        result,
+        token,
+        message: "User successfully logged in!",
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+);
+
+export default router;
