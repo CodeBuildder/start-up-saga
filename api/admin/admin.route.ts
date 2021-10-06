ea1117cbe3bb576f7e14f8ae24f19d3fb02052d2
whitedevil31 <magz3116@gmail.com>
2021-10-06 12:33:32 +0530

    Add filter by dates option

--- api/admin/admin.route.ts
+++ api/admin/admin.route.ts
@@ -1,77 +1,97 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { companyType } from "./admin.schema";
 import { verifiedAdmin } from "../middleware/auth";
-import { registerAdmin, loginAdmin, postCompanyDetails } from "./admin.controller";
+import {
+  registerAdmin,
+  loginAdmin,
+  postCompanyDetails,
+  getFilterCompanyDetails,
+} from "./admin.controller";
 
 import jwt from "jsonwebtoken";
 
 const router: Router = Router();
 
 router.post(
-    "/api/admin/register",
-    async (req: Request, res: Response, next: NextFunction) => {
-        const adminData = req.body;
+  "/api/admin/register",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const adminData = req.body;
 
-        try {
-            const result = await registerAdmin(adminData);
-            res.json(result).status(201);
-            next()
-        } catch (err) {
-            next(err);
-        }
+    try {
+      const result = await registerAdmin(adminData);
+      res.json(result).status(201);
+      next();
+    } catch (err) {
+      next(err);
     }
+  }
 );
 
 router.post(
-    "/api/admin/login",
-    async (req: Request, res: Response, next: NextFunction) => {
-        const { password, email } = req.body;
+  "/api/admin/login",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { password, email } = req.body;
 
-        try {
-            const result = await loginAdmin(email, password);
+    try {
+      const result = await loginAdmin(email, password);
 
-            const token = jwt.sign(
-                {
-                    email: email.email,
-                },
-                process.env.JWT_SECRET || "",
-                {
-                    expiresIn: "10d",
-                }
-            );
-
-            res.json({
-                result,
-                token,
-                message: "Hello, Welcome to Post.io",
-            });
-            next()
-        } catch (err) {
-            next(err);
+      const token = jwt.sign(
+        {
+          email: email.email,
+        },
+        process.env.JWT_SECRET || "",
+        {
+          expiresIn: "10d",
         }
+      );
+
+      res.json({
+        result,
+        token,
+        message: "Hello, Welcome to Post.io",
+      });
+      next();
+    } catch (err) {
+      next(err);
     }
+  }
 );
 
 router.post(
-    "/api/admin/company",
-    verifiedAdmin,
-    async (req: Request, res: Response, next: NextFunction) => {
-        const companyData = req.body as companyType
+  "/api/admin/company",
+  verifiedAdmin,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const companyData = req.body as companyType;
 
-
-        try {
-            const result = await postCompanyDetails(companyData);
-            res.status(201).json({
-                companyData,
-                message: "You have successfully posted the job!",
-            });
-
-        } catch (err) {
-            next(err);
-        }
+    try {
+      const result = await postCompanyDetails(companyData);
+      res.status(201).json({
+        companyData,
+        message: "You have successfully posted the job!",
+      });
+    } catch (err) {
+      next(err);
     }
+  }
 );
+interface filterData {
+  fromAddress: string;
+  toAddress: string;
+  date: Date;
+}
+router.get(
+  "/api/admin/company/filter",
+  verifiedAdmin,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const filterData = req.body as filterData;
 
-
+    try {
+      const result = await getFilterCompanyDetails(filterData);
+      res.json({ result });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 export default router;
