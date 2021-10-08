0b3d89774b496688621f8363928880d85bfe978b
KeshavRao-exe <73833127+KeshavRao-exe@users.noreply.github.com>
2021-10-08 16:12:24 +0530

    Dashboard Nav bar buttons

--- client/src/components/Admin/Orders.tsx
+++ client/src/components/Admin/Orders.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 import CONSTANTS from "../../constants/constants";
 // @ts-ignore
 import Loading from "react-fullscreen-loading";
-import { useHistory } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { BiMessageAdd } from "react-icons/bi";
+import { BiLogOut } from "react-icons/bi";
+import { ToastContainer, toast } from "react-toastify";
 interface companyOrder {
   userId: { username: String; email: string; phone: Number };
   toAddress: string;
@@ -12,8 +16,8 @@ interface companyOrder {
   weight: Number;
   price: Number;
 }
+
 const Orders = () => {
-  const history = useHistory();
   const [loaded, setLoaded] = useState(false);
   const [Order, setOrder] = useState<companyOrder[]>([]);
   useEffect(() => {
@@ -30,20 +34,47 @@ const Orders = () => {
 
     setLoaded(true);
   }, []);
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    toast.warn("Logging out !");
+    setTimeout(() => {
+      history.push("login");
+    }, 2000);
+  };
+  const history = useHistory();
   return (
     <div>
       {/* Navigation Bar */}
+      <ToastContainer />
       <div className="navbar  shadow-lg bg-purple-700 text-neutral-content h-18">
         <div className="flex-1 px-2 mx-2">
           <span className="text-lg font-bold">Start.exe</span>
-          <button
-            onClick={() => history.push("/admin/dashboard")}
-            className="m-2"
-          >
-            ADD COMPANY DATA
-          </button>
+          </div>
+          <div
+          className="btn btn-ghost btn-md rounded-btn flex  content-center "
+          onClick={() => history.push("dashboard")}
+        >
+          <div className="mt-1">
+          <IconContext.Provider value={{ size: "27px" }}>
+            <BiMessageAdd />
+          </IconContext.Provider>            
+          </div>
+
+          <pre> </pre>
+          ADD LISTING
         </div>
-      </div>
+        <a
+          className="btn btn-ghost btn-md rounded-btn flex  content-center"
+          onClick={logoutHandler}
+        >
+          <IconContext.Provider value={{ size: "26px" }}>
+            <BiLogOut />
+          </IconContext.Provider>
+          <pre> </pre>
+          SIGN OUT
+        </a>
+
+          </div>
 
       {/*Maiin page */}
       <div className="w-100 min-h-screen text-black bg-gray-100 ">
