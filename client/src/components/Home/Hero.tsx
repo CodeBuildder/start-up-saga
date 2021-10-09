9f03d898de95a069e1c68998c609ea30206de364
whitedevil31 <magz3116@gmail.com>
2021-10-09 23:27:15 +0530

    Align Hero home

--- client/src/components/Home/Hero.tsx
+++ client/src/components/Home/Hero.tsx
@@ -87,16 +87,19 @@ const Hero = () => {
 
       {/* Stats */}
       <div className="w-100 h-40 bg-purple-700 grid grid-flow-col justify-items-stretch py-4 px-4">
-        <div className="flex justify-center">
-          <IconContext.Provider value={{ size: "40px" }}>
-            <div>
-              <h2>
-                <BiNotepad />
-              </h2>
-            </div>
-          </IconContext.Provider>
-          <pre> </pre>
-          <h1 className="mb-5 text-3xl font-bold">Projects Done</h1>
+        <div className="flex flex-col justify-center  text-white justify-items-center items-center">
+          <div className="flex">
+            {" "}
+            <IconContext.Provider value={{ size: "40px" }}>
+              <div>
+                <h2>
+                  <BiNotepad />
+                </h2>
+              </div>
+            </IconContext.Provider>
+            <pre> </pre>
+            <h1 className="mb-5 text-3xl font-bold">Projects Done</h1>
+          </div>{" "}
           <p className="mb-5">320</p>
         </div>
         <div className="flex justify-center text-white">
@@ -111,7 +114,7 @@ const Hero = () => {
           <h1 className="mb-5 text-3xl font-bold">Clients Worldwide</h1>
           <p className="mb-5">320</p>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center text-white">
           <IconContext.Provider value={{ size: "40px" }}>
             <div>
               <h2>
@@ -123,7 +126,7 @@ const Hero = () => {
           <h1 className="mb-5 text-3xl font-bold">Owned Vehicles</h1>
           <p className="mb-5">320</p>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center text-white">
           <IconContext.Provider value={{ size: "40px" }}>
             <div>
               <h2>
@@ -138,44 +141,6 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Second Screen */}
-      <div className="hero min-h-screen w-100  bg-base-200">
-        <div className="flex flex-row justify-items-center items-center min-w-full ">
-          {" "}
-          <div className="w-1/2 flex ">
-            <h1 className="mb-5 text-5xl font-bold">Change content</h1>
-          </div>
-          <div className="flex-col w-1/2  flex-col">
-            <div className="w-50 h-50 flex flex-row justify-items-center items-stretch">
-              <div className="w-72 h-48  m-5 border-4 border-gray-600"></div>
-              <div className="w-72 h-48   m-5 border-4 border-gray-600"></div>
-            </div>
-            <div className="w-50 h-50 flex flex-row justify-items-center items-stretch">
-              <div className="w-72 h-48  m-5 border-4 border-gray-600"></div>
-              <div className="w-72 h-48   m-5 border-4 border-gray-600"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Third Screen */}
-      <div className="hero min-h-screen  bg-base-200">
-        <div className="flex-col hero-content lg:flex-row-reverse">
-          <img
-            src="https://picsum.photos/id/1005/600/600"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div>
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
       <footer className="p-10 footer bg-base-400 text-base-content">
         <div>
           <svg
